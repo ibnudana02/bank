b84b5d5493303bac443fd523255cf94443959c69
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-10-12 11:47:44 +0700

    20201012-01

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta property="fb:pages" content="742475696152707" />
     <title><?= $judul; ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
