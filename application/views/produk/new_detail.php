79e0855db9b1a48d9ef5b8e2e7e4008b53a7d13c
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-12-07 12:04:00 +0700

    20201207-04

--- application/views/produk/new_detail.php
+++ application/views/produk/new_detail.php
@@ -8,11 +8,8 @@
 <section id="page-content" class="sidebar-right">
     <div class="container">
         <div class="row">
-
             <div class="content col-lg-9">
-
                 <div id="blog" class="single-post">
-
                     <div class="post-item">
                         <div class="post-item-wrap">
                             <div class="post-image" data-animate="bounce">
@@ -42,79 +39,7 @@
                                 </div>
                                 <?php echo $row->deskripsi; ?>
                             </div>
-                            <div class="post-navigation">
-                                <a href="blog-single-slider.html" class="post-prev">
-                                    <div class="post-prev-title"><span>Previous Post</span>Post with a slider and lightbox</div>
-                                </a>
-                                <a href="blog-masonry-3.html" class="post-all">
-                                    <i class="icon-grid"> </i>
-                                </a>
-                                <a href="blog-single-video.html" class="post-next">
-                                    <div class="post-next-title"><span>Next Post</span>Post with YouTube Video</div>
-                                </a>
-                            </div>
-
-                            <!-- <div class="comments" id="comments">
-                                <div class="comment_number">
-                                    Comments <span>(2)</span>
-                                </div>
-                                <div class="comment-list">
-
-                                    <div class="comment" id="comment-1">
-                                        <div class="image"><img alt="" src="images/blog/author.jpg" class="avatar"></div>
-                                        <div class="text">
-                                            <h5 class="name">John Doe</h5>
-                                            <span class="comment_date">Posted at 15:32h, 06 December</span>
-                                            <a class="comment-reply-link" href="#">Reply</a>
-                                            <div class="text_holder">
-                                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div> -->
-
-                            <!-- <div class="respond-form" id="respond">
-                                <div class="respond-comment">
-                                    Leave a <span>Comment</span></div>
-                                <form class="form-gray-fields">
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <div class="form-group">
-                                                <label class="upper" for="name">Name</label>
-                                                <input class="form-control required" name="senderName" placeholder="Enter name" id="name" aria-required="true" type="text">
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <div class="form-group">
-                                                <label class="upper" for="email">Email</label>
-                                                <input class="form-control required email" name="senderEmail" placeholder="Enter email" id="email" aria-required="true" type="email">
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-4">
-                                            <div class="form-group">
-                                                <label class="upper" for="website">Website</label>
-                                                <input class="form-control website" name="senderWebsite" placeholder="Enter Website" id="website" aria-required="false" type="text">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="form-group">
-                                                <label class="upper" for="comment">Your comment</label>
-                                                <textarea class="form-control required" name="comment" rows="9" placeholder="Enter comment" id="comment" aria-required="true"></textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="form-group text-center">
-                                                <button class="btn" type="submit">Submit Comment</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div> -->
+                            <div class="seperator"><i class="fa fa-chevron-down"></i></div>
                         </div>
                     </div>
                 </div>
@@ -132,15 +57,12 @@
                 </div>
                 <div class="widget">
                     <div class="tabs">
-                        <ul class="nav nav-tabs" id="tabs-posts" role="tablist">
-                            <li class="nav-item">
+                        <ul class="nav nav-tabs nav-justified" id="tabs-posts" role="tablist">
+                            <!-- <li class="nav-item">
                                 <a class="nav-link active" id="home-tab" data-toggle="tab" href="#popular" role="tab" aria-controls="popular" aria-selected="true">Popular</a>
-                            </li>
+                            </li> -->
                             <li class="nav-item">
-                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#featured" role="tab" aria-controls="featured" aria-selected="false">Featured</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#recent" role="tab" aria-controls="recent" aria-selected="false">Recent</a>
+                                <a class="nav-link active" id="contact-tab" data-toggle="tab" href="#recent" role="tab" aria-controls="recent" aria-selected="false">Recent</a>
                             </li>
                         </ul>
                         <div class="tab-content" id="tabs-posts-content">
@@ -152,18 +74,6 @@
                                             <a href="#">A true story, that never been told!</a>
                                             <span class="post-date"><i class="icon-clock"></i> 6m ago</span>
                                             <span class="post-category"><i class="fa fa-tag"></i> Technology</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="tab-pane fade" id="featured" role="tabpanel" aria-labelledby="featured-tab">
-                                <div class="post-thumbnail-list">
-                                    <div class="post-thumbnail-entry">
-                                        <img alt="" src="images/blog/thumbnail/6.jpg">
-                                        <div class="post-thumbnail-content">
-                                            <a href="#">Beautiful nature, and rare feathers!</a>
-                                            <span class="post-date"><i class="icon-clock"></i> 24h ago</span>
-                                            <span class="post-category"><i class="fa fa-tag"></i> Lifestyle</span>
                                         </div>
                                     </div>
                                 </div>
@@ -181,14 +91,6 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="widget  widget-tags">
-                    <h4 class="widget-title">Tags</h4>
-                    <div class="tags">
-                        <a href="#">Design</a>
-                        <a href="#">Portfolio</a>
-                        <a href="#">Digital</a>
                     </div>
                 </div>
             </div>
--- application/views/template/new_header.php
+++ application/views/template/new_header.php
@@ -17,27 +17,25 @@
 
     <div class="body-inner">
 
-        <div id="topbar" class="d-none hidden-sm">
+        <div id="topbar" class="hidden-sm">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6 text-md-left text-center">
                         <ul class="top-menu">
-                            <li><a href="#">Phone: +1 (234) 567-890</a></li>
-                            <li><a href="#">Email: contact@inspiro-media.com</a></li>
+                            <li><a href="#">Phone: (0761)-63381</a></li>
+                            <li><a href="#">Email: unisritamabpr@yahoo.co.id</a></li>
                         </ul>
                     </div>
                     <div class="col-md-6 d-none d-sm-block">
                         <div class="social-icons social-icons-colored-hover">
                             <ul>
-                                <li class="social-facebook"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                                <li class="social-twitter"><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                <li class="social-google"><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
-                                <li class="social-pinterest"><a href="#"><i class="fab fa-pinterest"></i></a></li>
-                                <li class="social-vimeo"><a href="#"><i class="fab fa-vimeo"></i></a></li>
+                                <li class="social-facebook"><a href="https://www.instagram.com/bpr_unisritama"><i class="fab fa-facebook-f"></i></a></li>
+                                <li class="social-wa"><a href="https://wa.me/6282391031212"><i class="fab fa-whatsapp"></i></a></li>
+                                <li class="social-twitter"><a href="https://twitter.com/bpr_unisritama"><i class="fab fa-twitter"></i></a></li>
+                                <li class="social-instagram"><a href="https://www.instagram.com/bpr_unisritama/"><i class="fab fa-instagram"></i></a></li>
+                                <li class="social-facebook"><a href="https://www.instagram.com/bpr_unisritama/"><i class="fab fa-instagram"></i></a></li>
                                 <li class="social-linkedin"><a href="#"><i class="fab fa-linkedin"></i></a></li>
-                                <li class="social-dribbble"><a href="#"><i class="fab fa-dribbble"></i></a></li>
-                                <li class="social-youtube"><a href="#"><i class="fab fa-youtube"></i></a></li>
-                                <li class="social-rss"><a href="#"><i class="fa fa-rss"></i></a></li>
+                                <li class="social-youtube"><a href="https://www.youtube.com/channel/UCDXod80EPm_B-YndX4AMlIw"><i class="fab fa-youtube"></i></a></li>
                             </ul>
                         </div>
                     </div>
@@ -49,7 +47,7 @@
         <header id="header">
             <div class="header-inner">
                 <div class="container">
-                    <div id="logo"> <a href="index.html"><img src="<?php echo base_url('assets/'); ?>img/logo/bprs.png" class="logo-default" alt=""></a> </div>
+                    <div id="logo"> <a href="<?= base_url() ?>"><img src="<?php echo base_url('assets/'); ?>img/logo/bprs.png" class="logo-default" alt=""></a> </div>
                     <?php $this->load->view('template/new_menu'); ?>
                 </div>
             </div>
