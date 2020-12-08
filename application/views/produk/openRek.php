d00f2cc377ec65ac9c4198f7d0c2e570402257ef
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2020-12-08 18:24:19 +0700

    20201208-07

--- application/views/produk/openRek.php
+++ application/views/produk/openRek.php
@@ -4,145 +4,7 @@
             <div class="content col-md-11">
                 <div class="card">
                     <div class="card-body">
-                        <!-- <form id="wizard1" class="wizard">
-                            <h3>Account Information</h3>
-                            <div class="wizard-content">
-                                <div class="h5 mb-4">Please fill with your account details</div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="username">Username</label>
-                                        <input type="text" class="form-control" name="username" value="johnsmith" placeholder="Enter username">
-                                        <small id="emailHelp" class="form-text text-muted">We'll never share
-                                            your email with anyone else.</small>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="email">Email address</label>
-                                        <input type="email" class="form-control" name="email" value="johnsmith@email.com" placeholder="Enter your email">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="password">Password</label>
-                                        <div class="input-group show-hide-password">
-                                            <input class="form-control" name="password" value="password123" placeholder="Enter password" type="password">
-                                            <div class="input-group-append">
-                                                <span class="input-group-text"><i class="icon-eye-off" aria-hidden="true" style="cursor: pointer;"></i></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="password2">Confirm Password</label>
-                                        <div class="input-group show-hide-password">
-                                            <input class="form-control" name="password2" value="password123" placeholder="Enter password" type="password">
-                                            <div class="input-group-append">
-                                                <span class="input-group-text"><i class="icon-eye-off" aria-hidden="true" style="cursor: pointer;"></i></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <h3>Personal Information</h3>
-                            <div class="wizard-content">
-                                <div class="h5 mb-4">Please fill with your personal info</div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="name">Name</label>
-                                        <input type="text" class="form-control" name="name" value="John" placeholder="Enter your Name">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="surname">Surname</label>
-                                        <input type="text" class="form-control" name="surname" value="Smith" placeholder="Enter your Surname">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="gender">Gender</label>
-                                        <select class="form-control" name="gender">
-                                            <option>Select your gender</option>
-                                            <option>Female</option>
-                                            <option>Male</option>
-                                            <option>Rather not say</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="gender">Date of Birth</label>
-                                        <input class="form-control" type="date" value="2011-08-19" name="dateofbirth">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="telephone">Telephone</label>
-                                        <input class="form-control" type="tel" value="1-234-567-8910" name="telephone" placeholder="Enter your Telephone number">
-                                    </div>
-                                </div>
-                            </div>
-                            <h3>Mailing Address</h3>
-                            <div class="wizard-content">
-                                <div class="h5 mb-4">Mailing Address</div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="address">Address</label>
-                                        <input type="text" class="form-control" name="address" value="121 King Street" placeholder="Enter your Street Address">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="address2">Address 2</label>
-                                        <input type="text" class="form-control" name="address2" value="Apartament 2, Floor 1" placeholder="Enter your Apartment, studio, or floor">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="city">City</label>
-                                        <input type="text" class="form-control" name="city" value="Melburne" placeholder="Enter your City">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label>Zip Code:</label>
-                                        <input type="number" class="form-control" name="zip" placeholder="Enter Zip Code" value="123456">
-                                    </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="state">State</label>
-                                        <select name="state" class="form-control">
-                                            <option>Australia</option>
-                                            <option>Option 1</option>
-                                            <option>Option 2</option>
-                                            <option>Option 3</option>
-                                        </select>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="country">Country</label>
-                                        <select name="country" class="form-control">
-                                            <option>United States</option>
-                                            <option>Option 1</option>
-                                            <option>Option 2</option>
-                                            <option>Option 3</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <h3>Confirmation</h3>
-                            <div class="wizard-content">
-                                <div class="h5 mb-4">Confirmation</div>
-                                <p>Customize your experience by confirming your personalization settings and the
-                                    data
-                                    stored with your account. You can always learn more about these options,
-                                    adjust
-                                    them, and review your activity in your Account</p>
-                                <p>These settings apply wherever you are signed in to your new Account.</p>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" name="terms_conditions" id="terms_conditions1" class="custom-control-input">
-                                        <label class="custom-control-label" for="terms_conditions1">By checking
-                                            this
-                                            option, you agree to acceot with the <a href="#">Terms and
-                                                Conditions</a>.</label>
-                                    </div>
-                                </div>
-                            </div>
-                        </form> -->
-
                         <form id="wizard7" class="wizard needs-validation" data-style="1" novalidate>
-
                             <h3>Account Information</h3>
                             <div class="wizard-content">
                                 <div class="h5 mb-4">Please fill with your account details</div>
@@ -177,8 +39,6 @@
                                     </div>
                                 </div>
                             </div>
-
-
                             <h3>Personal Information</h3>
                             <div class="wizard-content">
                                 <div class="h5 mb-4">Please fill with your personal info</div>
@@ -214,8 +74,6 @@
                                     </div>
                                 </div>
                             </div>
-
-
                             <h3>Mailing Address</h3>
                             <div class="wizard-content">
                                 <div class="h5 mb-4">Mailing Address</div>
@@ -260,8 +118,6 @@
                                     </div>
                                 </div>
                             </div>
-
-
                             <h3>Confirmation</h3>
                             <div class="wizard-content">
                                 <div class="h5 mb-4">Confimration</div>
@@ -289,7 +145,6 @@
                                     </div>
                                 </div>
                             </div>
-
                         </form>
                     </div>
                 </div>
@@ -299,77 +154,10 @@
 </section>
 <script src="<?= base_url('assets/new/') ?>js/jquery.js"></script>
 <script src="<?= base_url('assets/new/') ?>plugins/validate/validate.min.js"></script>
-
 <link href="<?= base_url('assets/new/') ?>plugins/jquery-steps/jquery.steps.css" rel="stylesheet">
 <script src="<?= base_url('assets/new/') ?>plugins/jquery-steps/jquery.steps.min.js"></script>
 
 <script>
-    $('#wizard1').steps({
-        headerTag: 'h3',
-        bodyTag: '.wizard-content',
-        autoFocus: true,
-        enableAllSteps: true,
-        titleTemplate: '<span class="number">#index#</span><span class="title">#title#</span>',
-        onFinished: function(event, currentIndex) {
-            INSPIRO.elements.notification("Submited",
-                "Thank you, your account has been registed successfully", "success");
-        }
-    });
-    $('#wizard2').steps({
-        headerTag: 'h3',
-        bodyTag: '.wizard-content',
-        autoFocus: true,
-        enableAllSteps: true,
-        titleTemplate: '<span class="number">#index#</span><span class="title">#title#</span>',
-        onFinished: function(event, currentIndex) {
-            INSPIRO.elements.notification("Submited",
-                "Thank you, your account has been registed successfully", "success");
-        }
-    });
-    $('#wizard3').steps({
-        headerTag: 'h3',
-        bodyTag: '.wizard-content',
-        autoFocus: true,
-        enableAllSteps: true,
-        titleTemplate: '<span class="number">#index#</span><span class="title">#title#</span>',
-        onFinished: function(event, currentIndex) {
-            INSPIRO.elements.notification("Submited",
-                "Thank you, your account has been registed successfully", "success");
-        }
-    });
-    $('#wizard4').steps({
-        headerTag: 'h3',
-        bodyTag: '.wizard-content',
-        autoFocus: true,
-        enableAllSteps: true,
-        titleTemplate: '<span class="number">#index#</span><span class="title">#title#</span>',
-        onFinished: function(event, currentIndex) {
-            INSPIRO.elements.notification("Submited",
-                "Thank you, your account has been registed successfully", "success");
-        }
-    });
-    $('#wizard5').steps({
-        headerTag: 'h3',
-        bodyTag: '.wizard-content',
-        autoFocus: true,
-        enableAllSteps: true,
-        titleTemplate: '<span class="number">#index#</span><span class="title">#title#</span>',
-        onFinished: function(event, currentIndex) {
-            INSPIRO.elements.notification("Submited",
-                "Thank you, your account has been registed successfully", "success");
-        }
-    });
-    $('#wizard6').steps({
-        headerTag: 'h3',
-        bodyTag: '.wizard-content',
-        autoFocus: true,
-        enableAllSteps: true,
-        titleTemplate: '<span class="number">#index#</span><span class="title">#title#</span>',
-        onFinished: function(event, currentIndex) {
-            INSPIRO.elements.notification("Submited",
-                "Thank you, your account has been registed successfully", "success");
-        }
-    });
     //Advanced - with validation
     var wizard7 = $('#wizard7');
     wizard7.steps({
