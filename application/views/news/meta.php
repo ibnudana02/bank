3eb901b7f68507e5697c37b85809be52958581f1
Muhammad Ibnu Wardana <muhammadibnuwardana@gmail.com>
2021-02-05 15:23:41 +0700

    20210205-05

--- application/views/news/meta.php
+++ application/views/news/meta.php
@@ -1,7 +1,7 @@
 <meta property="fb:app_id" content="1283434705339233" />
 <meta property="og:url" content="<?= current_url() ?>" />
-<meta property="og:title" content="<?= $row->judul ?>" />
-<meta property="og:description" content="<?= htmlspecialchars_decode(substr($row->isi, 0, 30)) ?>" />
+<meta property="og:title" content="<?= ucwords($row->judul) ?>" />
+<meta property="og:description" content="<?= substr(ucfirst($row->isi), 0, 30) ?>" />
 <meta property="og:image" content="<?= base_url('upload/berita/' . $row->thumb); ?>" />
 <meta property="og:image:width" content="300" />
 <meta property="og:image:height" content="300" />
--- application/views/produk/meta.php
+++ application/views/produk/meta.php
@@ -1,8 +1,8 @@
 <meta property="fb:app_id" content="1283434705339233" />
 <meta property="og:url" content="<?= current_url() ?>" />
-<meta property="og:title" content="<?= $row->produk ?>" />
+<meta property="og:title" content="<?= ucwords($row->produk) ?>" />
 <meta property="og:site_name" content="Bank Unisritama" />
-<meta property="og:description" content="<?= htmlspecialchars(substr($row->deskripsi, 0, 30)) ?>" />
+<meta property="og:description" content="<?= substr(ucfirst($row->deskripsi), 0, 30) ?>" />
 <meta property="og:image" content="<?= base_url('upload/produk/' . $row->thumb); ?>" />
 <meta property="og:image:width" content="300" />
 <meta property="og:image:height" content="300" />
